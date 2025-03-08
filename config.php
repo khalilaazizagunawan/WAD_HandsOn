d166c0ef3a84b218b77907f112e453f1ead10961
jioxyjeje <jioxyjj@gmail.com>
2025-03-09 01:58:51 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ $databaseName = 'wad_handson';
 $databaseUsername = 'root';
 $databasePassword = '';
  
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+$mysqli =  mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
  
 ?>
